c5d83cc8a862bb0a03b77678c1f38ba37a785e70
AlvaroCarrion <alvarocarrion.git@gmail.com>
2026-02-04 17:21:02 +0100

    Subo esto para hacerlo luego en casa

---
+++ tema-4-css-avanzado/apartado-1-ej-1.js
@@ -0,0 +1,26 @@
+"use strict";
+
+window.onload = () => {
+    const inputRow = document.getElementsByName('gridRowGap')[0];
+    const inputColum = document.getElementsByName('gridColumnGap')[0];
+    const spanRow = document.getElementById('spangridRowGap');
+    const spanColumn = document.getElementById('spangridColumnGap');
+
+    inputRow.value = 1;
+    inputColum.value = 1;
+    spanRow.textContent = 1;
+    spanColumn.textContent = 1;
+
+    inputRow.addEventListener("input",(evento) => {
+        const valorCambiado = evento.target.value;
+        const spanInputRow = evento.target.previousElementSibling.firstElementChild;
+        spanInputRow.textContent = valorCambiado;
+        spanRow.textContent = valorCambiado;
+    });
+    inputColum.addEventListener("input",(evento) => {
+        const valorCambiado = evento.target.value;
+        const spanInputColumn = evento.target.previousElementSibling.firstElementChild;
+        spanInputColumn.textContent = valorCambiado;
+        spanColumn.textContent = valorCambiado;
+    });
+} //Fin del window onload()
